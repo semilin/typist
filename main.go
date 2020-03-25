4715fa4983ea4627818255e569122beb0f759759
Koen Westendorp <koensswestendorp@icloud.com>
2020-03-25 15:04:38 +0100

    improve result formatting and programming style
    - Added plural func to determine whether a 'error' should be pluralised.
    - Created a stat result func to centralise the code. It was copy pasted
    across two different funcs before :).
    - Improved general programming style,
    - and ran it through 'gofmt -w' to clean the file up even further.

--- main.go
+++ main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"bufio"
-	"os"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
+	"os"
+	"path/filepath"
 	"strings"
 	"time"
-	"io/ioutil"
-	"path/filepath"
-	"math/rand"
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	countdown(3); clear()
+	countdown(3)
+	clear()
 	wpm, errors := playRound(3)
-	fmt.Printf("Result: %g WPM | %d error(s)\n",wpm,errors)
+	fmt.Println("Result:", resultStats(wpm, errors))
 }
 
 func countdown(length int) {
-	for i:=length;i>0;i-- {
+	for i := length; i > 0; i-- {
 		clear()
 		fmt.Println(i)
 		time.Sleep(time.Second)
@@ -28,14 +29,14 @@ func countdown(length int) {
 }
 
 func playRound(rounds int) (float64, int) {
-	tWPM := float64(0)
-	tErrors := int(0)
-	
-	for i:=0;i<rounds;i++ {
+	var tWPM float64 = 0
+	var tErrors int = 0
+
+	for i := 0; i < rounds; i++ {
 		clear()
-		fmt.Printf(" %g WPM | %d error(s)\n\n\n",tWPM,tErrors)
+		fmt.Println(resultStats(tWPM, tErrors), "\n\n\n")
 		wpm, errors := ttest(getSentence())
-		
+
 		if tWPM != 0 {
 			tWPM = (tWPM + wpm) / 2
 		} else {
@@ -47,6 +48,14 @@ func playRound(rounds int) (float64, int) {
 	return tWPM, tErrors
 }
 
+func plural(n int) string {
+	if n == 1 {
+		return ""
+	} else {
+		return "s"
+	}
+}
+
 func ttest(s string) (float64, int) {
 	start := time.Now()
 	fmt.Println(" " + s)
@@ -54,7 +63,7 @@ func ttest(s string) (float64, int) {
 	t := time.Now()
 	elapsed := t.Sub(start)
 	wpm := calcWPM(s, elapsed)
-	errors := calcErrors(s,result)
+	errors := calcErrors(s, result)
 	return wpm, errors
 }
 
@@ -69,48 +78,57 @@ func input(s string) string {
 	return text
 }
 
+func resultStats(wpm float64, errorCount int) string {
+	return fmt.Sprintf(" %.1f WPM | %d error%s", wpm, errorCount, plural(errorCount))
+}
+
 func calcWPM(s string, elapsed time.Duration) float64 {
-	wpm := float64((float64(strings.Count(s,"")) / float64(elapsed.Seconds()) * 12))
+	chars := strings.Count(s, "")                      // count the number of characters in the string (sentence)
+	cps := float64(chars) / float64(elapsed.Seconds()) // calculate the characters per second
+	cpm := cps * 60                                    // calculate characters per minute
+	wpm := cpm / 5                                     // convert cpm to wpm (5 characters per word)
+
 	return wpm
 }
 
 func calcErrors(expected string, result string) int {
-	owords := strings.Split(expected," ")
-	rwords := strings.Split(result," ")
-	e := 0
+	expectedWords := strings.Split(expected, " ")
+	resultWords := strings.Split(result, " ")
+	errorCount := 0
 
-	for i:=0;i<len(owords);i++ {
-		if len(rwords) <= i {
-			return e + (len(owords) - len(rwords))
-		} else if owords[i] != rwords[i] {
-			e++
-		} 
+	for i := 0; i < len(expectedWords); i++ {
+		if len(resultWords) <= i {
+			return errorCount + (len(expectedWords) - len(resultWords))
+		} else if expectedWords[i] != resultWords[i] {
+			errorCount++
+		}
 	}
 
-	return e-1
+	return errorCount - 1
 }
 
 func getSentence() string {
 	path := directory()
 	raws, err := ioutil.ReadFile(path + "sentences/shakespeare")
-	if err != nil {log.Fatal(err)}
-	
-	sentences := strings.Split(string(raws),"\n")
-	sentence := rand.Intn(len(sentences)-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sentences := strings.Split(string(raws), "\n")
+	sentence := rand.Intn(len(sentences) - 1)
 	return sentences[sentence]
-	
-	
+
 }
 
-func directory() string{
-    ex, err := os.Executable()
+func directory() string {
+	ex, err := os.Executable()
 
-    if err != nil {
-        log.Fatal(err)
-    }
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    exPath := filepath.Dir(ex) + "/"
-    return exPath
+	exPath := filepath.Dir(ex) + "/"
+	return exPath
 }
 
 func clear() {
